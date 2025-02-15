6c29cdba981e61b526a4d069eb4cfe94b6c1da4a
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-02-15 03:07:03 +0100

    ignore

--- Client/app/page.js
+++ Client/app/page.js
@@ -1,10 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [message, setMessage] = useState('loading')
+ useEffect(()=>
+{
+  fetch("http://127.0.0.1:8080/api/home")
+  .then((response)=> response.json())
+  .then((data)=> setMessage(data.message))
+}, [])
+
   return (
     <div>
       <h1>
       Hello World! Pedram is coming as a fullstack ML engineer
+      {message}
     </h1>
     </div>
   );
